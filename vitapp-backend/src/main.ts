b84802afd2468b64ba01926057c872e6f3be6261
MANAVANANA <han-noa.MANAVANANA@etu.univ-amu.fr>
2026-04-15 11:18:48 +0200

    chore: configurer la protection Helmet et Cors du backend

--- vitapp-backend/src/main.ts
+++ vitapp-backend/src/main.ts
@@ -4,8 +4,24 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet());
-  app.enableCors();
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          styleSrc: ["'self'", "'unsafe-inline'"],
+          imgSrc: ["'self'", "data:", "validator.swagger.io"],
+          scriptSrc: ["'self'", "https:", "'unsafe-inline'"],
+        },
+      },
+    }),
+  );
+  app.enableCors({
+    origin: ['http://localhost:4200/', 'http://localhost:5173/', 'http://localhost:3002', 'http://localhost:3000/api','http://localhost:3000'], 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(3002);
   console.log('✅ Backend démarré sur http://localhost:3002');
 }
